600f3d49bff7ad53dc255ff17f6b08d8a7a4cce8
Ishant <ishantchauhan2305@gmail.com>
2020-05-14 08:04:30 -0500

    Added POST, PUT, PATCH, DELETE function to our APIView

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Profiles REST API
+
 Profiles REST API course code.
--- gate/admin.py
+++ gate/admin.py
@@ -3,5 +3,3 @@ from django.contrib import admin
 from gate import models
 
 admin.site.register(models.UserProfile)
-
-# Register your models here.
